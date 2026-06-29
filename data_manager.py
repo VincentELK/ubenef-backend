87b2d7346e8fe3531b3a3dce81611c3594854c00
VincentELK <vinidev@outlook.fr>
2026-06-29 11:10:35 +0200

    ajout du module logging dans le fichier de database, mise ne place du traitement des logs dans un fichier .log

--- data_manager.py
+++ data_manager.py
@@ -2,25 +2,30 @@ import json
 from json import JSONDecodeError
 from delivery_manager import Delivery
 import sqlite3
+import logging
 
-
+logging.basicConfig(filename='data_manager.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class DataBaseManager():
-    def __init__(self):
-        self.connection = sqlite3.connect("deliveries.db")
-        self.cursor = self.connection.cursor()
-        self.filepath = "courses.json"
+    def __init__(self) -> None:
+        self.db_path = "deliveries.db"
     
     def create_deliveries_table(self):
-        self.cursor.execute("""
-                            CREATE TABLE IF NOT EXISTS deliveries(
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            distance REAL NOT NULL,
-                            price REAL NOT NULL,
-                            duration REAL NOT NULL,
-                            date TEXT NOT NULL
-                            );""")
-        self.connection.commit()
+        try:
+            with sqlite3.connect(self.db_path) as connection:
+                cursor = connection.cursor()
+
+            cursor.execute("""
+                                CREATE TABLE IF NOT EXISTS deliveries(
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                distance REAL NOT NULL,
+                                price REAL NOT NULL,
+                                duration REAL NOT NULL,
+                                date TEXT NOT NULL
+                                );""")
+        except sqlite3.Error as e:
+            logging.error(f"Erreur lors de la creation de la table : {e}")
+
 
 
     def save_delivery(self, delivery_obj):
@@ -33,7 +38,7 @@ class DataBaseManager():
                              delivery_dict["delivery_duration"],
                              delivery_dict["delivery_date"],
                              )
-            with sqlite3.connect("deliveries.db")as connection:
+            with sqlite3.connect(self.db_path) as connection:
                 cursor = connection.cursor()
                 cursor.execute("""
                                     INSERT INTO deliveries(distance, price, duration, date) 
@@ -42,30 +47,41 @@ class DataBaseManager():
                   
 
         except sqlite3.Error as e:
-            return f"Erreur lors de l'enregistrement des données : {e}"
+            logging.error(f"Erreur lors de l'enregistrement des données : {e}")
+
         
     def get_deliveries(self):
-        delivery = self.cursor.execute("""SELECT id, distance, price, duration, date FROM deliveries""" )
-        deliveries_object_list = []
-        for row in delivery:
-            db_id = row[0]
-            db_distance = row[1]
-            db_price = row[2]
-            db_duration = row[3]
-            db_date = row[4]
-            delivery_object = Delivery(db_distance, db_price, db_duration, db_date, db_id)
-            deliveries_object_list.append(delivery_object)
-        return deliveries_object_list
+
+        try :
+            with sqlite3.connect(self.db_path) as connection:
+                cursor = connection.cursor()
+
+                delivery = cursor.execute("""SELECT id, distance, price, duration, date FROM deliveries""" )
+                deliveries_object_list = []
+                for row in delivery:
+                    db_id = row[0]
+                    db_distance = row[1]
+                    db_price = row[2]
+                    db_duration = row[3]
+                    db_date = row[4]
+                    delivery_object = Delivery(db_distance, db_price, db_duration, db_date, db_id)
+                    deliveries_object_list.append(delivery_object)
+                return deliveries_object_list
+        except sqlite3.Error as e:
+            logging.error(f"Erreur lors de la récupération des données : {e}")
+            return []
     
     def delete_delivery(self, input_delivery_id):
         try:
-            self.cursor.execute("DELETE FROM deliveries WHERE id = ?", (input_delivery_id,))
-            self.connection.commit()
-
-            if self.cursor.rowcount > 0:
-                return True
-            return False
-        except sqlite3.Error:
+            with sqlite3.connect(self.db_path) as connection:
+                cursor = connection.cursor()
+                cursor.execute("DELETE FROM deliveries WHERE id = ?", (input_delivery_id,))
+                
+                if cursor.rowcount > 0:
+                    return True
+                return False
+        except sqlite3.Error as e:
+            logging.error(f"Erreur lors de la suppression de livraison : {e}")
             return False
 
 
